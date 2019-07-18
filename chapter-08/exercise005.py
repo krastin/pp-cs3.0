c547b78c9d8caade9d5c9fe249024b194f06220e
Krastin Krastev <krastin@hashicorp.com>
2019-07-18 09:52:27 +0200

    chapter-08 done

--- chapter-08/exercise005.py
+++ chapter-08/exercise005.py
@@ -1,1 +1,10 @@
 alkaline_earth_metals = [4, 12, 20, 38, 56, 88]
+
+radium = alkaline_earth_metals[-1]
+print(radium)
+radium = alkaline_earth_metals[5]
+print(radium)
+
+print(len(alkaline_earth_metals))
+
+print(max(alkaline_earth_metals))
