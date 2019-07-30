b84320554be0d45879565df23c75a93b5271470b
Krastin Krastev <krastin@hashicorp.com>
2019-07-30 10:03:02 +0200

    chapter-10

--- chapter-10/exercise04.py
+++ chapter-10/exercise04.py
@@ -1,6 +1,21 @@
 from typing import TextIO
 from io import StringIO
-import time_series
+
+def skip_header(reader: TextIO) -> str:
+        """Skip the header in reader and return the first real piece of data.
+        >>> infile = StringIO('Example\\n# Comment\\n# Comment\\nData line\\n')
+        >>> skip_header(infile)
+        'Data line\\n'
+        """
+        # Read the description line
+        line = reader.readline()
+        # Find the first non-comment line
+        line = reader.readline()
+        while line.startswith('#'):
+                line = reader.readline()
+        # Now line contains the first real piece of data
+        return line
+
 def find_largest(line: str) -> int:
     """Return the largest value in line, which is a whitespace-delimited string
     of integers that each end with a '.'.
@@ -25,7 +40,7 @@ def process_file(reader: TextIO) -> int:
     >>> process_file(infile)
     100
     """
-    line = time_series.skip_header(reader).strip()
+    line = skip_header(reader).strip()
     # The largest value so far is the largest on this first line of data.
     largest = find_largest(line)
     # Check the rest of the lines for larger values.
